5274817bcb3d8a7d9a28ffe384fcb608c37a507c
kenju <priv.kenju@gmail.com>
2014-07-02 21:58:28 +0900

    none

--- iconv_test.go
+++ iconv_test.go
@@ -1,9 +1,46 @@
 package goiconv
 
 import (
+	"bytes"
 	"syscall"
 	"testing"
 )
+
+func iconv(from, to string, inbuf []byte, t *testing.T) (dst []byte) {
+	iconv, err := NewIConv(from, to)
+	if err != nil {
+		t.Errorf("NewIConv(%s, %s) failed. err=%s", from, to, err)
+	}
+	defer iconv.Close()
+
+	abuf := make([]byte, 0)
+	outbufLen := 1
+	outbuf := make([]byte, outbufLen)
+	for len(inbuf) > 0 {
+		inbuf, outbuf, err = iconv.IConv(inbuf, outbuf)
+		if len(outbuf) > 0 {
+			abuf = append(abuf, outbuf...)
+		}
+		if err != nil {
+			if err == syscall.E2BIG {
+				outbufLen++
+				outbuf = make([]byte, outbufLen)
+			} else {
+				t.Fatalf("IConv() failed. err=%s", err)
+			}
+		}
+	}
+
+	return abuf
+}
+
+func TestUtf8ToUtf8(t *testing.T) {
+	in := []byte("日本語")
+	out := iconv("utf8", "utf8", in, t)
+	if !bytes.Equal(in, out) {
+		t.Error("unexpected output returned")
+	}
+}
 
 func TestIConv(t *testing.T) {
 	// source data. 日本語 in utf8
